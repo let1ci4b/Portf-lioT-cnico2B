53b8ecfc6737fc81d771204d4d67e9764f7320b4
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2021-06-10 11:47:31 -0300

    Create vetor3.sh

---
+++ FundamentosTI/exemplos/vetor3.sh
@@ -0,0 +1,2 @@
+# Autora: Letícia Beatriz Souza
+# Descrição: Pedir para o usuário nomes de pessoas utilizando loop. Apenas parar de pedir a próxima pessoa quando o usuário digitar um “código de parada” da sua escolha. Colocar esses nomes de pessoas em um vetor e ao final dizer a quantidade de pessoas digitadas.
