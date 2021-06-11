b19a502662fd4e0c2507f865cf0fe6eb9303694f
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2021-06-10 23:58:51 -0300

    Update vetor3.sh

--- FundamentosTI/exemplos/vetor3.sh
+++ FundamentosTI/exemplos/vetor3.sh
@@ -1,2 +1,22 @@
 # Autora: Letícia Beatriz Souza
 # Descrição: Pedir para o usuário nomes de pessoas utilizando loop. Apenas parar de pedir a próxima pessoa quando o usuário digitar um “código de parada” da sua escolha. Colocar esses nomes de pessoas em um vetor e ao final dizer a quantidade de pessoas digitadas.
+#!/bin/bash
+i=0
+x=0
+while [ $x -ne 1 ]
+do
+        echo "Digite um nome:"
+        read nome
+        if [ $nome = "parar" ]
+        then
+                x=1
+                echo ""
+                echo "Nomes digitados: ${vetor[@]}"
+                echo "${#vetor[@]} pessoas foram digitadas"
+        else
+                x=2
+                echo ""
+        fi
+        vetor[$i]=$nome
+        i=$(($i+1))
+done
