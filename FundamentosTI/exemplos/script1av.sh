3676ca2869caa37043a637aa4427767fb603591d
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2021-08-05 09:36:58 -0300

    Update script1av.sh

--- FundamentosTI/exemplos/script1av.sh
+++ FundamentosTI/exemplos/script1av.sh
@@ -1,5 +1,31 @@
+# Autora: Letícia Beatriz Souza
+# Descrição: Faça um script que receba como parâmetro um nome de um arquivo ou diretório e diga quais permissões o usuário dono tem.
 #!/bin/bash
-echo "digite o nome do arquivo"
-read arq
-var=$(ls -l $arq)
-echo "$var"
+info=${ls -ld $1}
+permr=${info:1:1}
+permw=${info:2:1}
+permx=${info:3:1}
+
+if [[ $permr == "r" ]]
+then
+  echo "O arquivo ou diretório tem permissão de leitura!"
+else
+  echo "O arquivo ou diretório não tem permissão de leitura!"
+fi
+
+if [[ $permw == "w" ]]
+then
+  echo "O arquivo ou diretório tem permissão de escrita!"
+else
+  echo "O arquivo ou diretório não tem permissão de escrita!"
+fi
+
+if [[ $permw == "w" ]]
+then
+  echo "O arquivo ou diretório tem permissão de execução!"
+else
+  echo "O arquivo ou diretório não tem permissão de execução!"
+fi
+
+
+  
