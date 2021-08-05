ee0af0741080e2297bd9b69cb964894e040b2ff7
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2021-08-05 09:25:42 -0300

    Update script2av.sh

--- FundamentosTI/exemplos/script2av.sh
+++ FundamentosTI/exemplos/script2av.sh
@@ -1,3 +1,5 @@
+# Autora: Letícia Beatriz Souza
+# Descrição: Fazer um script para imprimir a lista de frutas com seus respectivos preços, em ordem alfabética.
 #!/bin/bash
 var=$(cut -d" " -f1 /etc/passwd)
 echo "$var"
