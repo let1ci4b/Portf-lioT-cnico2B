bbad66c55ff657f778cd51a125bc00b6e91c6305
Letícia Beatriz <leticia_b_souza@estudante.sc.senai.br>
2021-08-12 10:30:05 -0300

    exemplos para revisao

---
+++ FundamentosTI/exemplos/scriptrevisao1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "digite um número"
+read num
+
+if [ $num -gt 0 ]
+then
+        echo "$num é positivo"
+
+elif [ $num -lt 0 ]
+then
+        echo "$num é negativo"
+
+else
+        echo "$num é nulo"
+
+fi
