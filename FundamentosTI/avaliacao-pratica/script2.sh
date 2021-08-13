31fcbbacfcc05fb8b2204a57afab6a24a0ce1ada
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2021-08-13 10:08:24 -0300

    Update script2.sh

--- FundamentosTI/avaliacao-pratica/script2.sh
+++ FundamentosTI/avaliacao-pratica/script2.sh
@@ -1,3 +1,5 @@
+# Autora: Letícia Beatriz Souza
+# Descrição: Fazer um script que receba como parâmetro um nome de um arquivo ou diretório e diga quais permissões outros usuários tem (usuários que não sejam donos nem do grupo dono).
 #!/bin/bash
 ls
 echo "escolha um arquivo ou diretorio"
@@ -46,22 +48,22 @@ ls -ld $arq | cut -c10
 read permx
 
 if [[ $permr == "r" ]]
-	then
-		  echo "Outros usuários tem permissão de leitura!"
-		  else
-			    echo "Outros usuários não tem permissão de leitura!"
-			    fi
+then
+	echo "Outros usuários tem permissão de leitura!"
+else
+	echo "Outros usuários não tem permissão de leitura!"
+fi
 
-			    if [[ $permw == "w" ]]
-				    then
-					      echo "Outros usuários tem permissão de escrita!"
-					      else
-						        echo "Outros usuários não tem permissão de escrita!"
-							fi
+if [[ $permw == "w" ]]
+then
+	echo "Outros usuários tem permissão de escrita!"
+else
+	echo "Outros usuários não tem permissão de escrita!"
+fi
 
-							if [[ $permx == "x" ]]
-								then
-									  echo "Outros usuários tem permissão de execução!"
-									  else
-										    echo "Outros usuários não tem permissão de execução!"
-										    fii
+if [[ $permx == "x" ]]
+then
+	echo "Outros usuários tem permissão de execução!"
+else
+	echo "Outros usuários não tem permissão de execução!"
+fi
