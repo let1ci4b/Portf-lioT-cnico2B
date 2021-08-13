59d6d089ce35835f78bd28df272f5978cee4f9e7
Letícia Beatriz <leticia_b_souza@estudante.sc.senai.br>
2021-08-13 10:26:26 -0300

    scripts avaliacao

--- FundamentosTI/avaliacao-pratica/script1.sh
+++ FundamentosTI/avaliacao-pratica/script1.sh
@@ -1,2 +1,14 @@
 # Autora: Letícia Beatriz Souza
 # Descrição: Fazer um script que retorne todos os nomes dos usuários do sistema que não são humanos.
+
+#!/bin/bash
+val=$(cut -d":" -f3 /etc/passwd)
+num_user=0
+for i in $val
+do
+        if [[ $i -gt 0 && $i -lt 1000 ]]
+        then
+                num_user=$((num_user+1))
+        fi
+done
+echo $num_user
